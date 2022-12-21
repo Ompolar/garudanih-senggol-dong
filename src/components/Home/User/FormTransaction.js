20bb14b33169a6bd4c67c330c0942b8d708428c5
yusronab <yusron.arly@gmail.com>
2022-12-21 22:13:03 +0700

    todo: Implement react-select on input seat

--- src/components/Home/User/FormTransaction.js
+++ src/components/Home/User/FormTransaction.js
@@ -1,12 +1,15 @@
+import { AirlineSeatReclineExtra, AssignmentInd, Pin } from "@mui/icons-material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Card, Col, Form, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import Select from "react-select";
 
 export default function FormTransaction() {
     const { ticketId } = useParams()
     // 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
     const [data, setData] = useState(null)
+    const [numberOptions, setNumberOptions] = useState([])
     const [requestBody, setRequestBody] = useState({
         ktp: "",
         orderBy: "",
@@ -26,7 +29,20 @@ export default function FormTransaction() {
         }).catch((err) => {
             console.log(err.message)
         })
+
     }, [ticketId])
+
+    useEffect(() => {
+        if (data) {
+            let arr = []
+            for (let i = 0; i < data.ticket.totalChair; i++) {
+                const found = data.ticket.bookingBy.some(el => el.numChair === i);
+
+                if (!found) arr.push({ value: i, label: i });
+            }
+            setNumberOptions(arr)
+        }
+    }, [data])
 
     const onChangeHandler = (e) => {
         setRequestBody({ ...requestBody, [e.target.name]: e.target.value })
@@ -59,17 +75,31 @@ export default function FormTransaction() {
             {data ? (
                 <>
                     <h2>ticket {ticketId}</h2>
-                    <Form.Control type="text" name="ktp" value={requestBody.ktp} onChange={(e) => onChangeHandler(e)} placeholder="Nomor KTP . . ." />
-                    <Form.Control type="text" name="orderBy" value={requestBody.orderBy} onChange={(e) => onChangeHandler(e)} placeholder="Masukan nama . . ." />
-                    <p>kursi yang telah dipesan</p>
-                    <div className="d-flex">
-                        {data.ticket.bookingBy.map((user, index) => {
-                            return (
-                                <p key={index} className="bg-primary rounded p-1 text-white mx-1">{user.numChair}</p>
-                            )
-                        })}
-                    </div>
-                    <Form.Control type="text" name="numChair" value={requestBody.numChair} onChange={(e) => onChangeHandler(e)} placeholder="Pilih nomor kursi . . ." onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()} maxLength={3} />
+                    <Row>
+                        <Col md="4">
+                            <div className="d-flex mb-2">
+                                <Pin />
+                                <label className="ms-2">Identify Number</label>
+                            </div>
+                            <Form.Control type="text" name="ktp" value={requestBody.ktp} onChange={(e) => onChangeHandler(e)} placeholder="Nomor KTP . . ." />
+                        </Col>
+                        <Col md="5">
+                            <div className="d-flex mb-2">
+                                <AssignmentInd />
+                                <label className="ms-2">Booking As</label>
+                            </div>
+                            <Form.Control type="text" name="orderBy" value={requestBody.orderBy} onChange={(e) => onChangeHandler(e)} placeholder="Masukan nama . . ." />
+                        </Col>
+                        <Col md="3">
+                            <div className="d-flex mb-2">
+                                <AirlineSeatReclineExtra />
+                                <label className="ms-2">Seat Number</label>
+                            </div>
+                            {numberOptions.length !== 0 ? (
+                                <Select options={numberOptions} placeholder="Choose number . . ." />
+                            ) : ""}
+                        </Col>
+                    </Row>
                     <Row>
                         {data.returnTicket.length !== 0 ? <p>Pesan juga tiket pulang</p> : <p>Saat ini tiket pulang tidak tersedia</p>}
                         {data.returnTicket.map((ticket, index) => {
